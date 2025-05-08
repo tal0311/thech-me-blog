e8ff743a4be638388389398f402f38cddbfd8fcf
tal0311 <tal.amit0311@gmail.com>
2025-05-08 23:17:22 +0300

    update vite config do build to public

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  build: {
+    outDir: '../backend/public',
+    emptyOutDir: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
